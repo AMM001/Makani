c7a34c31adcfbb7d2b6e6ecfe4d37845a1cb1c17
menem <abdelmenim.mohamed97@gmail.com>
2018-06-08 13:52:42 +0200

    solve issues in constraints in profile and side menu screen

--- Makani/Controllers/SplashScreenViewController.swift
+++ Makani/Controllers/SplashScreenViewController.swift
@@ -22,7 +22,7 @@ class SplashScreenViewController: UIViewController {
         
         let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         let activityIndicatorView = NVActivityIndicatorView(frame: frame,
-                                                            type: NVActivityIndicatorType.ballSpinFadeLoader)
+                                                            type: NVActivityIndicatorType.ballBeat)
         activityIndicatorView.padding = 30
         
         self.fcview.addSubview(activityIndicatorView)
